a8c2ead76a60cf97e199da8c6b74c01680cf1c88
PutriJoeliya17 <putri.joelya@gmail.com>
2026-08-25 23:34:36 +0700

    final schema

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -4,52 +4,52 @@
 CREATE TABLE dim_demographics (
     demographic_id SERIAL PRIMARY KEY,
     gender VARCHAR(50),
-    age_broad VARCHAR(50),
+    agebroad VARCHAR(50),
     citizenship VARCHAR(100),
-    country_of_exploitation VARCHAR(100)
+    CountryOfExploitation VARCHAR(100)
 );
 
 -- 2. TABEL DIMENSI: Metode Pelaku (Means)
 CREATE TABLE dim_means (
     means_id SERIAL PRIMARY KEY,
-    means_debt_bondage INT,
-    means_threats INT,
-    means_abuse INT,
-    means_false_promises INT,
-    means_drugs_alcohol INT,
-    means_deny_basic_needs INT,
-    means_excessive_work_hours INT,
-    means_withhold_docs INT
+    meansDebtBondageEarnings INT,
+    meansThreats INT,
+    meansAbusePsyPhySex INT,
+    meansFalsePromises INT,
+    meansDrugsAlcohol INT,
+    meansDenyBasicNeeds INT,
+    meansExcessiveWorkHours INT,
+    meansWithholdDocs INT
 );
 
 -- 3. TABEL DIMENSI: Jenis & Sektor Eksploitasi
 CREATE TABLE dim_exploitation (
     exploitation_id SERIAL PRIMARY KEY,
-    is_forced_labour INT,
-    is_sexual_exploit INT,
-    is_other_exploit INT,
-    sector_agriculture INT,
-    sector_construction INT,
-    sector_domestic_work INT,
-    sector_hospitality INT,
-    sector_prostitution INT,
-    sector_pornography INT
+    isForcedLabour INT,
+    isSexualExploit INT,
+    isOtherExploit INT,
+    sectorOfLabourAgriculture INT,
+    sectorOfLabourConstruction INT,
+    sectorOfLabourDomesticWork INT,
+    sectorOfLabourHospitality INT,
+    sectorOfSexProstitution INT,
+    sectorOfSexPornography INT
 );
 
 -- 4. TABEL DIMENSI: Hubungan Perekrut
 CREATE TABLE dim_recruiter (
     recruiter_id SERIAL PRIMARY KEY,
-    recruiter_intimate_partner INT,
-    recruiter_friend INT,
-    recruiter_family INT,
-    recruiter_other INT
+    recruiterRelationIntimatePartner INT,
+    recruiterRelationFriend INT,
+    recruiterRelationFamily INT,
+    recruiterRelationOther INT
 );
 
 -- 5. FACT TABLE: Tabel Utama yang Menghubungkan Semua Dimensi (Foreign Keys)
 CREATE TABLE fact_trafficking_cases (
     case_id SERIAL PRIMARY KEY,
-    year_of_registration INT,
-    traffick_months INT,
+    yearOfRegistration INT,
+    traffickMonths INT,
     demographic_id INT REFERENCES dim_demographics(demographic_id),
     means_id INT REFERENCES dim_means(means_id),
     exploitation_id INT REFERENCES dim_exploitation(exploitation_id),
@@ -61,3 +61,5 @@ select * from dim_means;
 select * from dim_recruiter;
 select * from dim_exploitation;
 select * from fact_trafficking_cases;
+
+drop table dim_demographics, dim_means, dim_recruiter, dim_exploitation, fact_trafficking_cases;
