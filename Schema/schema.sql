3c0c298ffa80f42877d6bd2936ac8feaa58e92bb
PutriJoeliya17 <putri.joelya@gmail.com>
2026-08-26 21:46:03 +0700

    up

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -12,37 +12,37 @@ CREATE TABLE dim_demographics (
 -- 2. TABEL DIMENSI: Metode Pelaku (Means)
 CREATE TABLE dim_means (
     means_id SERIAL PRIMARY KEY,
-    meansDebtBondageEarnings INT,
-    meansThreats INT,
-    meansAbusePsyPhySex INT,
-    meansFalsePromises INT,
-    meansDrugsAlcohol INT,
-    meansDenyBasicNeeds INT,
-    meansExcessiveWorkHours INT,
-    meansWithholdDocs INT
+    meansDebtBondageEarnings BOOLEAN,
+    meansThreats BOOLEAN,
+    meansAbusePsyPhySex BOOLEAN,
+    meansFalsePromises BOOLEAN,
+    meansDrugsAlcohol BOOLEAN,
+    meansDenyBasicNeeds BOOLEAN,
+    meansExcessiveWorkHours BOOLEAN,
+    meansWithholdDocs BOOLEAN
 );
 
 -- 3. TABEL DIMENSI: Jenis & Sektor Eksploitasi
 CREATE TABLE dim_exploitation (
     exploitation_id SERIAL PRIMARY KEY,
-    isForcedLabour INT,
-    isSexualExploit INT,
-    isOtherExploit INT,
-    sectorOfLabourAgriculture INT,
-    sectorOfLabourConstruction INT,
-    sectorOfLabourDomesticWork INT,
-    sectorOfLabourHospitality INT,
-    sectorOfSexProstitution INT,
-    sectorOfSexPornography INT
+    isForcedLabour BOOLEAN,
+    isSexualExploit BOOLEAN,
+    isOtherExploit BOOLEAN,
+    sectorOfLabourAgriculture BOOLEAN,
+    sectorOfLabourConstruction BOOLEAN,
+    sectorOfLabourDomesticWork BOOLEAN,
+    sectorOfLabourHospitality BOOLEAN,
+    sectorOfSexProstitution BOOLEAN,
+    sectorOfSexPornography BOOLEAN
 );
 
 -- 4. TABEL DIMENSI: Hubungan Perekrut
 CREATE TABLE dim_recruiter (
     recruiter_id SERIAL PRIMARY KEY,
-    recruiterRelationIntimatePartner INT,
-    recruiterRelationFriend INT,
-    recruiterRelationFamily INT,
-    recruiterRelationOther INT
+    recruiterRelationIntimatePartner BOOLEAN,
+    recruiterRelationFriend BOOLEAN,
+    recruiterRelationFamily BOOLEAN,
+    recruiterRelationOther BOOLEAN
 );
 
 -- 5. FACT TABLE: Tabel Utama yang Menghubungkan Semua Dimensi (Foreign Keys)
